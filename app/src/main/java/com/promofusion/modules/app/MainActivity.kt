0d0bbdd7a990ea822a9b2981b6e9f8f068003a8e
lenghak <125646707+Lenghak@users.noreply.github.com>
2023-11-18 15:38:30 +0700

    Added colors, themes, & typo

--- app/src/main/java/com/promofusion/modules/app/MainActivity.kt
+++ app/src/main/java/com/promofusion/modules/app/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.promofusion.common.ui.theme.PromoFusionTheme
 
@@ -18,7 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             PromoFusionTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -31,6 +37,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+            .fillMaxWidth(),
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.Center
     )
 }
 
