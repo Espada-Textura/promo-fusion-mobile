0d5921278918db0bdc405a716e1e54fd70d3289b
lenghak <125646707+Lenghak@users.noreply.github.com>
2023-12-18 13:19:54 +0700

    Extract reusuable components of nav-item

--- app/src/main/java/com/promofusion/common/ui/components/navigations/BottomNavigationBar.kt
+++ app/src/main/java/com/promofusion/common/ui/components/navigations/BottomNavigationBar.kt
@@ -4,14 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +18,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,77 +39,73 @@ fun BottomNavigationBar(
             modifier = Modifier
                 .fillMaxWidth()
                 .background(color = MaterialTheme.colorScheme.background),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically,
         ) {
             NavigationRouteItems.forEachIndexed { index, item ->
 
                 if (item.route === "scan") {
-                    Row(
+
+                    BottomNavigationItem(
+                        selected = bottomNavState == index,
+                        onClick = {
+                            bottomNavState = index
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(
+                                    item.unselectedIcon
+                                ), contentDescription = item.title
+                            )
+                        },
                         modifier = Modifier
-                            .wrapContentSize(align = Alignment.Center)
+                            .fillMaxWidth()
+                            .weight(1f),
+                        containerModifier = Modifier
                             .size(64.dp)
                             .background(
-                                MaterialTheme.colorScheme.primary,
-                                shape = RoundedCornerShape(64.dp)
+                                MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(64.dp)
                             ),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        NavigationBarItem(modifier = Modifier.background(
-                            color = Color.Transparent
-                        ),
-
-                            selected = bottomNavState == index, onClick = {
-                                bottomNavState = index
-                            }, icon = {
-                                Icon(
-                                    imageVector = ImageVector.vectorResource(
-                                        if (bottomNavState == index) item.selectedIcon
-                                        else item.unselectedIcon
-                                    ),
-                                    contentDescription = item.title,
-                                    modifier = Modifier.padding(0.dp)
-                                )
-                            },
-
-                            colors = NavigationBarItemDefaults.colors(
-                                unselectedIconColor = MaterialTheme.colorScheme.onPrimary,
-                                selectedIconColor = MaterialTheme.colorScheme.onPrimary,
-                                selectedTextColor = MaterialTheme.colorScheme.onPrimary,
-                                indicatorColor = MaterialTheme.colorScheme.primary
-                            ),
-
-
-                            alwaysShowLabel = false
+                        label = { },
+                        colors = NavigationBarItemDefaults.colors(
+                            unselectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                            selectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                            selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            indicatorColor = MaterialTheme.colorScheme.primary
                         )
-                    }
+                    )
+
                 } else {
-                    NavigationBarItem(modifier = Modifier.background(
-                        color = MaterialTheme.colorScheme.background
-                    ), selected = bottomNavState == index, onClick = {
-                        bottomNavState = index
-                    }, icon = {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(
-                                if (bottomNavState == index) item.selectedIcon
-                                else item.unselectedIcon
-                            ), contentDescription = item.title
-                        )
-                    },
 
+                    BottomNavigationItem(
+                        selected = bottomNavState == index,
+                        onClick = {
+                            bottomNavState = index
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(
+                                    if (bottomNavState == index) item.selectedIcon
+                                    else item.unselectedIcon
+                                ), contentDescription = item.title
+                            )
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        containerModifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        label = {
+                            item.title?.let { Text(text = it) }
+                        },
                         colors = NavigationBarItemDefaults.colors(
                             unselectedIconColor = MaterialTheme.colorScheme.scrim,
                             selectedIconColor = MaterialTheme.colorScheme.primary,
                             selectedTextColor = MaterialTheme.colorScheme.primary,
                             indicatorColor = MaterialTheme.colorScheme.background
                         ),
-
-                        alwaysShowLabel = false,
-
-                        label = {
-                            item.title?.let { Text(text = it) }
-                        })
+                    )
                 }
             }
         }
--- app/src/main/java/com/promofusion/common/ui/components/navigations/BottomNavigationItem.kt
+++ app/src/main/java/com/promofusion/common/ui/components/navigations/BottomNavigationItem.kt
@@ -1,2 +1,40 @@
 package com.promofusion.common.ui.components.navigations
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun BottomNavigationItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier,
+    containerModifier: Modifier,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)?,
+    colors: NavigationBarItemColors,
+) {
+    Row(
+        modifier = containerModifier
+            .wrapContentSize(align = Alignment.Center),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        NavigationBarItem(
+            modifier = modifier,
+            selected = selected, onClick = onClick, icon = icon,
+            colors = colors,
+            alwaysShowLabel = false,
+            label = label,
+            enabled = enabled,
+        )
+    }
+
+}
