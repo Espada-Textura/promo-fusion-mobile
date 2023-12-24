c8b833e0d4abce93695437eb8c3ba245d1c04456
ChanvisalYorn <89696454+YornChanvisal@users.noreply.github.com>
2023-12-24 11:09:39 +0700

    [Build Gradle] - add splash screen jetpack lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,6 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
     implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
+    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
 }
