d058dce84ce2c48aeb102aa4fb184228e0490c4f
lenghak <125646707+Lenghak@users.noreply.github.com>
2024-01-03 13:03:36 +0700

    Edited some theme & packages

--- app/src/main/java/com/promofusion/common/ui/theme/Theme.kt
+++ app/src/main/java/com/promofusion/common/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -53,7 +52,7 @@ private val darkModeScheme = darkColorScheme(
 
 private val lightModeScheme = lightColorScheme(
 
-    background = Color.White,
+    background = Neutral100,
     onBackground = Neutral950,
 
     surface = Neutral50,
--- app/src/main/java/com/promofusion/modules/app/fragments/home/views/HomeScreen.kt
+++ app/src/main/java/com/promofusion/modules/app/fragments/home/views/HomeScreen.kt
@@ -5,7 +5,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
     Text(text = "Home")
-
 }
