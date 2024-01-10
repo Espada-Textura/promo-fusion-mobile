f6f1b607700898269b3b9dd6404d73840ad7a6b1
ChanvisalYorn <89696454+YornChanvisal@users.noreply.github.com>
2024-01-10 23:46:24 +0700

    [Coupon Card] - Change to using Elevation Card from Material UI 3

--- app/src/main/java/com/promofusion/common/components/CouponCard.kt
+++ app/src/main/java/com/promofusion/common/components/CouponCard.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,28 +23,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.promofusion.common.constants.CouponStatus
+import com.promofusion.common.theme.displayFontFamily
 
 @Composable
-fun CouponCardView(
+fun CouponElevationCard (
     couponId: Int,
     shopName: String,
     promotion: String,
     expiredAt: String,
     status: CouponStatus
 ) {
-    Surface(
+    ElevatedCard (
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
         modifier = Modifier
             .fillMaxWidth()
-            .height(128.dp)
-            .padding(0.dp)
-            .shadow(8.dp),
-        shape = RoundedCornerShape(16.dp),
-        color = Color.White,
+            .height(128.dp),
     ) {
         Row(
 //            horizontalAlignment = Alignment.CenterHorizontally,
@@ -49,27 +57,61 @@ fun CouponCardView(
             horizontalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource(id = com.promofusion.R.drawable.ic_coupon_fill),
+                painter = painterResource(id = com.promofusion.R.drawable.ic_promos_logo_foreground),
                 contentDescription = "Shop Logo"
             )
             Spacer(modifier = Modifier.width(8.dp))
             Spacer(modifier = Modifier.width(8.dp))
             Column(
+                modifier = Modifier
+                    .width(200.dp),
                 horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text(text = shopName)
-                Text(text = promotion + " off")
-                Text(text = expiredAt)
+                Text(
+                    text = shopName,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 0.6.sp,
+                    textAlign = TextAlign.Center
+                )
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = promotion,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        letterSpacing = 0.6.sp,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "off",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Normal,
+                        letterSpacing = 0.6.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                Text(
+                    text = expiredAt,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                    letterSpacing = 0.6.sp,
+                    textAlign = TextAlign.Center
+                )
             }
             Spacer(modifier = Modifier.width(8.dp))
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .padding(8.dp)
+                    .align(Alignment.Top)
+                    .padding(0.dp)
                     .background(if (status == CouponStatus.Valid) Color.Red else if (status == CouponStatus.Used) Color.Green else Color.Magenta)
-                    .fillMaxWidth()
-
+                    .fillMaxWidth(),
+                
             ) {
                 Text(
                     text = if (status == CouponStatus.Valid) "Valid" else if (status == CouponStatus.Used) "Used" else if (status == CouponStatus.Expired) "Expired" else "Invalid",
@@ -84,7 +126,7 @@ fun CouponCardView(
 @Preview
 @Composable
 fun ReusableCardViewPreview() {
-    CouponCardView(
+    CouponElevationCard(
         1,
         "Starbuck",
         "25%",
