5a606b85c17f44f9cc65bf3441d2f14bb76bcd38
ChanvisalYorn <89696454+YornChanvisal@users.noreply.github.com>
2024-01-12 23:03:24 +0700

    [Setting|Profile] - Add Profile Info Section

---
+++ app/src/main/java/com/promofusion/modules/main/fragments/settings/views/ProfileInfoSection.kt
@@ -0,0 +1,49 @@
+package com.promofusion.modules.main.fragments.settings.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun ProfileInfoSection (
+    Name: String,
+    Email: String,
+) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        AsyncImage(
+            model = "https://example.com/image.jpg",
+            contentDescription = "Profile Picture",
+        )
+        Text(
+            text = Name,
+            fontWeight = FontWeight.ExtraBold,
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+                .padding(0.dp, 8.dp, 0.dp, 0.dp)
+        )
+        Text(
+            text = Email,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.scrim,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+                .padding(0.dp, 8.dp)
+        )
+    }
+}
